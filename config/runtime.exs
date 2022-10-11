c8accab2c3f83b07b7cc65b5afb851970df108a5
lud <ludovic@demblans.com>
2022-10-11 11:17:38 +0200

    ignore .env

--- config/runtime.exs
+++ config/runtime.exs
@@ -10,18 +10,7 @@ Dotenvy.source([".env"])
 # any compile-time configuration in here, as it won't be applied.
 # The block below contains prod specific runtime configuration.
 
-# ## Using releases
-#
-# If you use `mix release`, you need to explicitly enable the server
-# by passing the PHX_SERVER=true when you start it:
-#
-#     PHX_SERVER=true bin/vlx start
-#
-# Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
-# script that automatically sets the env var above.
-if System.get_env("PHX_SERVER") do
-  config :vlx, VlxWeb.Endpoint, server: true
-end
+config :vlx, VlxWeb.Endpoint, server: true
 
 config :vlx, :media,
   dir: env!("VLX_MEDIA_DIR", :string!),
